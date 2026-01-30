d995c29e16f271c580a8758d1df34990f64ef174
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2026-01-30 21:26:40 +0530

    Remove unnecessary blank line in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,6 @@ import { useTranslation } from 'next-i18next';
 export default function Footer() {
   const { t } = useTranslation('common');
 
-  
   return (
     <footer className="bg-gray-800 text-white p-4 mt-8 no-print">
       <div className="container mx-auto text-center">
