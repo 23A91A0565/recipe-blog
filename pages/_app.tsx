c928889f85de10b745a27bba2e79928ff8453c58
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2026-01-30 21:31:54 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import '../styles/globals.css';
 
+
 export default appWithTranslation(function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 });
